6f6c2755f3e26ffcb2804a2b5464d959d04403c4
Dmitriy <dvorobjevredstar@mail.ru>
2022-06-28 07:27:11 +0500

    Изменил дизайн Home

--- src/pages/Home/Header.tsx
+++ src/pages/Home/Header.tsx
@@ -1,13 +1,16 @@
 import { FC } from "react";
 import styles from "./header.module.scss";
+import createIMG from "./create.png";
+import profileIMG from "./profile.png";
 
 export const Header: FC = () => {
   return (
     <div className={styles.body}>
       <p className={styles.logo}>{"<COMPO / >"}</p>
-      <p className={styles.description}>
-        {"Бесплатный магазин компонентов для вашего сайта"}
-      </p>
+      <div className={styles.interface}>
+        <img src={createIMG} alt="create" className={styles.create} />
+        <img src={profileIMG} alt="profile" className={styles.profile} />
+      </div>
     </div>
   );
 };
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,10 @@
 import { FC } from "react";
-import { Card } from "./Card";
 import { Header } from "./Header";
 
 export const Home: FC = () => {
   return (
     <div>
       <Header />
-      <Card />
     </div>
   );
 };
